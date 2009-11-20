ac00ba5c33f472f5aca3d1cc31e2a24341a8d65f
gaude.emmanuel <gaude.emmanuel@47de7700-cebd-11de-bc73-ddba90c8dc6e>
2009-11-20 13:50:51 +0000

    compi séance mercredi

--- trunk/TP2/ex2/R0/inc/Type.h
+++ trunk/TP2/ex2/R0/inc/Type.h
@@ -1,30 +1,25 @@
 #ifndef TYPE_DEF
 #define TYPE_DEF
+
+#include <cstring>
+#include "exceptions.h"
 /**
  * Classe de gestion d'un type du langage Pascal.
  */
 class Type {
 	private:
-		/** Constante correspondant au type courant. */
-		TypeId _id;
+		/** Liste des types de base du Pascal. */
+		static const char* names[];
+
 		/** Chaîne correspondante au nom du type dans le langage. */
 		char* _name;
 	public:
-		/** Enumération des types connus. */
-		enum TypeId {
-			BOOLEAN,	/**< Type booléen. */
-			CHAR,		/**< Type caractère. */
-			INTEGER,	/**< Type entier. */
-			REAL,		/**< Type réel. */
-			STRING		/**< Type chaîne de caractères. */
-		};
-		
 		/**
 		 * Constructeur.
 		 *
-		 * @param[in] id Id de type possible.
+		 * @param[in] name Nom du type.
 		 */
-		Type(const TypeId id);
+		Type(const char* name) throw();
 		
 		/** 
 		 * Obtenir nom du type courant.
@@ -33,29 +28,12 @@ class Type {
 		 */
 		char* getName() const;
 
-		/** 
-		 * Obtenir du du type courant.
-		 * 
-		 * @return Id du type courant.
-		 */
-		TypeId getId() const;
-
 		/**
 		 * Changer type courant.
 		 *
-		 * @param[in] id Id de type possible.
+		 * @param[in] name Nom du type.
 		 */
-		void setId(const TypeId id);
-};
-/**
- * Classe d'Exception d'un type inconnu.
- */
-class UnknownTypeException: public std::exception {
-	public:
-		/** Message d'erreur à retourner. */
-		const char* what() const throw() {
-			return "Probleme : Type inconnu !";
-		}
+		void setName(const char* name) throw();
 };
 
 #endif
---
+++ trunk/TP2/ex2/R0/src/exceptions.cpp
@@ -0,0 +1,13 @@
+#include "exceptions.h"
+
+const char* UnknownBasicTypeException::what() const throw() {
+	return "Probleme : Type de base inconnu !";
+}
+
+const char* NullPointerException::what() const throw() {
+	return "Probleme : pointeur NULL rencontré !";
+}
+
+const char* AllocationException::what() const throw() {
+	return "Probleme rencontre lors de l'allocation !";
+}
